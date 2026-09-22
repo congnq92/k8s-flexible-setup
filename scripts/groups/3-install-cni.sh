50d351d97884df358e978045bfa973511a25f452
congnq92 <congnq92@gmail.com>
2026-09-22 19:42:25 +0600

    fix scripts 2

--- scripts/groups/3-install-cni.sh
+++ scripts/groups/3-install-cni.sh
@@ -15,13 +15,20 @@ devModeExitIfEnabled "${BASH_SOURCE[0]}"
 
 readonly KUBECONFIG_PATH="${KUBECONFIG_PATH:-/etc/kubernetes/admin.conf}"
 readonly FLANNEL_MANIFEST_URL="${FLANNEL_MANIFEST_URL:-https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml}"
+local_node_pod_cidr=''
 
 require_root
 require_command kubectl
 [[ -r "${KUBECONFIG_PATH}" ]] || fail "Kubernetes admin kubeconfig not found: ${KUBECONFIG_PATH}"
 
-local_node_pod_cidr="$(kubectl --kubeconfig "${KUBECONFIG_PATH}" get node "$(hostname)" -o jsonpath='{.spec.podCIDR}')"
-if [[ "${local_node_pod_cidr}" != "${POD_NETWORK_CIDR}" ]]; then
+log 'Waiting for a Pod CIDR on the local node'
+for _ in {1..30}; do
+    local_node_pod_cidr="$(kubectl --kubeconfig "${KUBECONFIG_PATH}" get node "$(hostname)" -o jsonpath='{.spec.podCIDR}')"
+    [[ -n "${local_node_pod_cidr}" ]] && break
+    sleep 1
+done
+
+if [[ -z "${local_node_pod_cidr}" ]]; then
     fail $'Flannel requires Pod network CIDR 10.244.0.0/16.\nThis cluster was initialized without it. Reset this new cluster,\nthen run Install groups again.'
 fi
 
